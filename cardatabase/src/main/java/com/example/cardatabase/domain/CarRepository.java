a5cc839a607d5053642d3bf67610e7eb1916fc39
nabell0 <pmy738170@gmail.com>
2025-09-23 11:05:30 +0900

    feat: h2 인메모리 설정관련코드

--- cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/CarRepository.java
@@ -1,4 +1,9 @@
 package com.example.cardatabase.domain;
 
-public interface CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+
 }
--- cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/example/cardatabase/domain/Owner.java
@@ -1,4 +1,31 @@
 package com.example.cardatabase.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
+@ToString
 public class Owner {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerId;
+
+    @NonNull
+    private final String firstName;
+    @NonNull
+    private final String lastName;
+
+    // 소유자는 다수의 차들을 가질 수 있기 때문에 Collections를 사용.
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
+
+    // Lombok 사용하지 않고, 기본 생성자 및 ownerId를 제외한 RequiredArgsConstructor
+    // Lombok 사용하지 않고, 각 field에 대한 getter setter를 생성할 것
+
 }
